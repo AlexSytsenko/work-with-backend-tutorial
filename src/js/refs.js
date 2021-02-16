2b9079aaf43d484f0574fdcbc5032f1f3d8716cf
AlexSytsenko <sytsenko@gmail.com>
2021-02-16 15:39:38 +0200

    create an environment for lesson 13

--- src/js/refs.js
+++
@@ -1,5 +0,0 @@
-const articlesContainer = document.querySelector('.js-articles');
-const searchForm = document.querySelector('.js-search-form');
-   
-
-export default {articlesContainer, searchForm};
